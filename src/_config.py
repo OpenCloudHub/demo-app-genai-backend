c882eef6b17ce8b6dcc26af22a4be55712ca50c5
reinhud <lrdev+github@proton.me>
2025-11-25 00:01:24 +0000

    stuff

--- src/_config.py
+++ src/_config.py
@@ -1,8 +1,14 @@
-from pydantic import Field
-from pydantic_settings import BaseSettings
+from pydantic import BaseModel, Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class APIConfig(BaseSettings):
+class BaseServiceSettings(BaseSettings):
+    """Allows extra environment variables to be ignored."""
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+class APIConfig(BaseServiceSettings):
     """Configuration for RAG API"""
 
     prompt_version: int | None = None  # None = @production
@@ -10,7 +16,7 @@ class APIConfig(BaseSettings):
     port: int = Field(default=8000)
 
 
-class DatabaseConfig(BaseSettings):
+class DatabaseConfig(BaseServiceSettings):
     """Configuration for database connection"""
 
     # PgVector settings
@@ -21,34 +27,34 @@ class DatabaseConfig(BaseSettings):
     top_k: int = 10
 
 
-class PromptConfig(BaseSettings):
+class PromptConfig(BaseServiceSettings):
     """Configuration for prompt management"""
 
-    mlflow_tracking_uri: str = "https://mlflow.ai.internal.opencloudhub.org"
+    mlflow_tracking_uri: str = "https://mlflow.internal.opencloudhub.org"
     dvc_repo: str = "https://github.com/OpenCloudHub/data-registry"
     eval_data_path: str = "data/opencloudhub-readmes/rag-evaluation/questions.csv"
     prompt_name: str = "readme-rag-prompt"
     prompt_version: int | None = None  # Use @production alias
 
 
-class ModelConfig(BaseSettings):
+class ModelConfig(BaseServiceSettings):
     """Configuration for RAG chain"""
 
     # LLM settings
-    llm_base_url: str = "https://api.opencloudhub.org/models/qwen-0.5b/v1"
+    llm_base_url: str = "https://api.opencloudhub.org/models/base/qwen-0.5b/v1"
     llm_model: str = "qwen2.5-0.5b-instruct"
     api_key: str = "dummy"  # Not used but required by ChatOpenAI
     # Embedding model
     embedding_model: str = "sentence-transformers/all-MiniLM-L6-v2"
 
 
-class EvalConfig(BaseSettings):
+class EvalConfig(BaseServiceSettings):
     """Configuration for RAG evaluation"""
 
     eval_data_version: str = "opencloudhub-readmes-rag-evaluation-v1.0.0"
 
 
-class Config(BaseSettings):
+class Config(BaseModel):
     """Configuration for RAG API and evaluation"""
 
     api: APIConfig = APIConfig()
